099367f623e16c42cd0d24aee4b8a46d34c7d090
David Wesst <867084+davidwesst@users.noreply.github.com>
2021-10-19 03:35:20 +0000

    setup testing scaffold for azure functions

---
+++ articles/tests/index.test.js
@@ -0,0 +1,29 @@
+const httpFunction = require('../index');
+const context = require('./defaultContext')
+
+beforeEach(() => {
+    jest.clearAllMocks();
+})
+
+test('Http trigger should return known text', async () => {
+
+    const request = {
+        query: { name: 'Bill' }
+    };
+
+    await httpFunction(context, request);
+
+    expect(context.log.mock.calls.length).toBe(1);
+    expect(context.res.body).toEqual('Hello Bill');
+});
+
+test('Article HTTP Trigger should return known text correctly', async () => {
+    const request = {
+        query: { name: 'Bill' }
+    };
+
+    await httpFunction(context, request);
+
+    expect(context.log.mock.calls.length).toBe(1);
+    expect(context.res.body).toEqual('Hello Bill');
+});
