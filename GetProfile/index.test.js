846fd51a32e27923ad4087bc203edcc58e924e61
David Wesst <david@cocobokostudios.com>
2021-10-21 18:30:42 +0000

    added profile function scaffold #1

--- GetProfile/index.test.js
+++ GetProfile/index.test.js
@@ -4,10 +4,8 @@ beforeEach(() => {
     jest.clearAllMocks();
 });
 
-test.todo('Should return 200 and article with matching slug');
-test.todo('Should return 200 with profile with slug provided is \'about\'');
-test.todo('Should return 404 and no content when no article with slug exists');
-test.todo('Should return 200 and list of most recent 20 articles if no slug is provided');
+test.todo('Should return 200 with profile');
+test.todo('Should return 500 when connection to GitHub fails');
 
 test('Article HTTP Trigger should return known text correctly', async () => {
     const context = {
@@ -23,5 +21,5 @@ test('Article HTTP Trigger should return known text correctly', async () => {
     await httpFunction(context, request);
 
     expect(context.log.mock.calls.length).toBe(1);
-    expect(context.res.body).toEqual(`Hello Bill. Your slug is: ${context.bindingData.slug}`);
+    expect(context.res.body).toEqual(`Hello, Bill. This HTTP triggered function executed successfully.`);
 });
